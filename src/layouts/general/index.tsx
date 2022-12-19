16cb3dbb7950227b4dcfa7f514aaaa25d4937033
Mikael Brevik <mikaelbre@gmail.com>
2022-12-19 18:03:42 +0100

    feat: adds transitioning background

--- src/layouts/general/index.tsx
+++ src/layouts/general/index.tsx
@@ -1,5 +1,6 @@
 import favicon from '@variant/profile/lib/logo/favicon.png';
 import Head from 'next/head';
+import { useState, useRef, useEffect } from 'react';
 import PageHero from 'src/components/page-hero';
 import { and } from 'src/utils/css';
 import { LayoutProps } from '../signature';
@@ -13,11 +14,13 @@ export default function GeneralLayout({
   noSidebar = false,
   children,
 }: LayoutProps) {
+  const [val, ref] = usePercentageInView<HTMLDivElement>();
+
   // const subHeadings = toc[0]?.children.map((c) => c.value) ?? [];
   const classes = and(style.main, !noSidebar ? style.main__sidebar : undefined);
 
   return (
-    <div className={classes}>
+    <div className={classes} style={{ ['--bg-opacity' as any]: 1 - val }}>
       <Head>
         <title>{frontmatter?.title ?? title}</title>
         <link rel="icon" href={favicon} />
@@ -34,7 +37,9 @@ export default function GeneralLayout({
         />
       </Head>
 
-      <PageHero />
+      <div ref={ref}>
+        <PageHero />
+      </div>
 
       {/* <nav className={and(style.nav, style.nav__active)}>
         <section className={style.nav__inner}>
@@ -138,4 +143,52 @@ export default function GeneralLayout({
       </footer>
     </div>
   );
+}
+
+function usePercentageInView<T extends HTMLElement>(): [
+  number,
+  React.RefObject<T>,
+] {
+  const [value, setValue] = useState<number>(1);
+  const ref = useRef<T>(null);
+
+  useEffect(() => {
+    const percentageSeen = throttle(() => {
+      if (!ref.current) return;
+      const node = ref.current;
+      const scrollTop = window.scrollY;
+      const elementOffsetTop = node.offsetTop;
+      const elementHeight = node.offsetHeight;
+      const distance = scrollTop - elementOffsetTop;
+
+      const percentage = Math.round(distance / (elementHeight / 100));
+      const newValue = Number(
+        (Math.min(100, Math.max(0, percentage)) / 100).toPrecision(1),
+      );
+      setValue(newValue);
+    }, 30);
+
+    percentageSeen();
+    window.addEventListener('scroll', percentageSeen, {
+      passive: true,
+    });
+    return () => window.addEventListener('scroll', percentageSeen);
+  }, [ref]);
+
+  return [value, ref];
+}
+
+function throttle(func: () => void, timeout: number) {
+  let ready: boolean = true;
+  return () => {
+    if (!ready) {
+      return;
+    }
+
+    ready = false;
+    func();
+    setTimeout(() => {
+      ready = true;
+    }, timeout);
+  };
 }
