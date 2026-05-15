68c76fe2a2b088d0e93a8139f2e46cfe2447cf82
Mikael Brevik <mikaelbre@gmail.com>
2026-05-15 20:21:22 +0200

    modernize mdx handling

--- src/layouts/handbook/index.tsx
+++ src/layouts/handbook/index.tsx
@@ -1,60 +1,11 @@
-import React from 'react';
 import Layout from '../general';
 import Book from 'src/components/book';
-import { MDXProvider } from '@mdx-js/react';
-import slugify from 'slugify';
 import { LayoutProps } from '../signature';
-
-import style from './handbook.module.css';
-
-function createLinkable(el: 'h2' | 'h3' | 'h4') {
-  return ({ children, ...props }: React.JSX.IntrinsicElements[typeof el]) => {
-    const textContent = getNodeText(children);
-    const slug = slugify(textContent, { lower: false });
-    const childList = React.Children.toArray(children);
-    return React.createElement(
-      el,
-      {
-        ...props,
-        id: slug,
-        key: slug,
-      },
-      childList.concat(
-        <a className={style.anchor} key="link" href={`#${slug}`}>
-        </a>,
-      ),
-    );
-  };
-}
-const LinkableH2 = createLinkable('h2');
-const LinkableH3 = createLinkable('h3');
-const LinkableH4 = createLinkable('h4');
-
-const components = {
-  h2: LinkableH2,
-  h3: LinkableH3,
-  h4: LinkableH4,
-};
 
 export default function HandbookLayout({ children, ...props }: LayoutProps) {
   return (
-    <MDXProvider components={components}>
-      <Layout {...props}>
-        <Book filename="">{children}</Book>
-      </Layout>
-    </MDXProvider>
+    <Layout {...props}>
+      <Book filename="">{children}</Book>
+    </Layout>
   );
-}
-
-function getNodeText(node: React.ReactNode): string {
-  if (typeof node == 'string') return node;
-  if (typeof node == 'number') return String(node);
-  if (typeof node == 'boolean') return '';
-  if (typeof node == 'bigint') return String(node);
-  if (node == null) return '';
-  if (Array.isArray(node)) return node.map(getNodeText).join('');
-  if (React.isValidElement(node)) {
-    return getNodeText((node.props as { children?: React.ReactNode }).children);
-  }
-  return '';
 }
